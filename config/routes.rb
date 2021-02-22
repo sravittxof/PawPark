6e628c47ce3f937f50b10757087b5028520a4bc7
sravittxof <travis.fox90@gmail.come>
2021-02-21 19:09:45 -0500

    Add has_secure_password to user model, uncomment bcrypt gem in Gemfile, add Pry to Gemfile and bundle. Limit Park resources to index, show, edit and update.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
+  #custom routes above generator provided routes
+  
   resources :playdates
   resources :visits
-  resources :parks
+  resources :parks, only: [:index, :show, :edit, :update]
   resources :dogs
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
