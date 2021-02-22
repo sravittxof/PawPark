6924054c91db7e43b1363e42a350fb5a9804a4b7
sravittxof <travis.fox90@gmail.come>
2021-02-21 19:42:24 -0500

    Create view for creating new user on signup. Use form_for in view. Add new, create and show routes to User controller. Define user_params private method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to '/show'
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(:username, :email, :passowrd)
+    end
+
 end
