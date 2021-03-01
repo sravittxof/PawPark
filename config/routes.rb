6bafae65c3ce4e6233cde61d23f1a313ea812e66
sravittxof <travis.fox90@gmail.come>
2021-02-28 20:15:22 -0500

    In config/routes.rb: limit dogs resources nested under user to index, new and create actions only.

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   resources :parks, only: [:index, :show, :edit, :update]
   resources :dogs, only: [:index, :show]
   resources :users do
-    resources :dogs
+    resources :dogs, only: [:index, :new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
