a27765b990a23a9c9e7a937bb19c6508ee544dd1
sravittxof <travis.fox90@gmail.come>
2021-03-01 02:36:54 -0500

    In sessions_controller.rb: uncomment sessions#new and sessions#create actions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,24 @@
 class SessionsController < ApplicationController
+    #skip_before_action :verify_authenticity_token, only: :create
+    
+    def omniauth
+        user = User.from_omniauth(request.env['omniauth.auth'])
+        if user.valid?
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            redirect_to '/login'
+        end
+    end
 
     def new
     end
-    
+
     def create
         if session[:user_id]
             redirect_to '/'
-#        elsif params[:username].nil? || params[:username].empty?
-#            redirect_to '/login'
+##        elsif params[:username].nil? || params[:username].empty?
+##            redirect_to '/login'
         else
             user = User.find_by(username: params[:username])
             if user && user.authenticate(params[:password])
