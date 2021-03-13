1b4ee399e0776ec7006f37040103464aa46de806
sravittxof <travis.fox90@gmail.come>
2021-03-13 11:59:00 -0500

    In dogs_controller.rb: write new and create actions.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,18 +1,38 @@
 class DogsController < ApplicationController
 
+
     def index
-        if params[:user_id] && @user = User.find_by(id: params[:user_id])
-            @dogs = @user.dogs.all
-        else
-            @dogs = Dog.all
-        end
+        # if params[:user_id] && @user = User.find_by(id: params[:user_id])
+        #     @dogs = @user.dogs.all
+        # else
+        #     @dogs = Dog.all
+        # end
+        @dogs = Dog.all
     end
 
     def show
+        binding.pry
         @dog = Dog.find(params[:id])
     end
 
+    def new
+        @dog = Dog.new
+    end
 
+    def create
+        binding.pry
+        @dog = Dog.new(dog_params)
+        if @dog.save
+            redirect_to dog_path(@dog)
+        else
+            render :new
+        end
+    end
 
+    private
+
+    def dog_params
+        params.permit(:dog)
+    end
 
 end
