d8ee6534db9e6b85d16719321099b5d96657691c
sravittxof <travis.fox90@gmail.come>
2021-03-13 11:21:25 -0500

    In models/visit.rb: scope active_visits to show only when active is true. Create visiting_dog_name method.

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -3,4 +3,22 @@ class Visit < ApplicationRecord
   belongs_to :park
   has_many :visit_playdates
   has_many :playdates, through: :visit_playdates
+
+
+  scope :active_visit, -> { where(active_visit: true) }
+
+  def visiting_dog_name
+    self.dog.name
+  end
+
+  # def dog_name=(name)
+  #   self.dog = 
+  # end
+
+  # def dog_name
+  #   self.name
+  # end
+
+
+
 end
