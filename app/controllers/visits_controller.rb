414fb812adf62bf4c1ebcae1096a5ce6086abf12
sravittxof <travis.fox90@gmail.come>
2021-03-13 11:29:41 -0500

    In visits_controller.rb: modify edit action to find single visit selected for editing in parks/show page rather than collection of all current_visits.

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,5 +1,13 @@
 class VisitsController < ApplicationController
 
+    def index
+    
+    end
+
+    def show
+
+    end
+
     def new
         @visit = Visit.new(park_id: params[:park_id])
     end
@@ -7,6 +15,7 @@ class VisitsController < ApplicationController
     def create
         binding.pry
         @visit = Visit.new(visit_params)
+        @visit.active_visit = true
         binding.pry
         if @visit.save
             redirect_to park_path(@visit.park)
@@ -16,15 +25,32 @@ class VisitsController < ApplicationController
     end
 
     def edit
+        binding.pry
+        @visit = Visit.find_by(id: params[:id])
+        # current_user.dogs.each do |dog|
+        #     visit = dog.visits.last
+        #     if visit.active_visit
+        #         @visit << visit #if visit.active_visit
+        #     end
+        # end
+
     end
 
     def update
+        binding.pry
+        @visit = Visit.find(params[:id])
+        binding.pry
+        if @visit.update(active_visit: false)
+            redirect_to parks_path
+        else
+            render park_path(@visit.park)
+        end
     end
 
     private
 
     def visit_params
-        params.require(:visit).permit(:dog_id, :park_id, :active_visit)
+        params.require(:visit).permit(:dog_id, :park_id, :active_visit, :id)
     end
 
 end
