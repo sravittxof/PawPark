55dadf6f53e4d6463aea834d1f85f5ddd3b1c00e
sravittxof <travis.fox90@gmail.come>
2021-03-18 14:50:07 -0400

    Remove check to see if session[:id] exists from create method. This will be handled by application_controller method logged_in?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,19 +15,13 @@ class SessionsController < ApplicationController
     end
 
     def create
-        if session[:user_id]
-            redirect_to '/'
-##        elsif params[:username].nil? || params[:username].empty?
-##            redirect_to '/login'
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to "/users/#{user.id}"
         else
-            user = User.find_by(username: params[:username])
-            if user && user.authenticate(params[:password])
-                session[:user_id] = user.id
-                redirect_to "/users/#{user.id}"
-            else
-                flash[:message] = "Please enter correct username and password."
-                redirect_to '/login'
-            end
+            flash[:message] = "Please enter correct username and password."
+            redirect_to '/login'
         end
     end
 
