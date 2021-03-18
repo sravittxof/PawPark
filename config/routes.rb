9ddd93d6c151b235784c174bc08ab7b7a26ba0ca
sravittxof <travis.fox90@gmail.come>
2021-03-18 17:37:44 -0400

    Remove nesting dogs under users. Delete get '/' route

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,19 @@
 Rails.application.routes.draw do
-  
   root "welcome#home"
-  get '/' => 'welcome#home'
-  get '/auth/:provider/callback', to: 'sessions#omniauth' #, via: [:get, :post]
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   #custom routes above generator provided routes
   
-  
-
-  #resources :parks, only: [:index, :show, :edit, :update]
   resources :parks do
-    resources :visits, only: [:show, :new]#[:new, :create, :edit, :update]
+    resources :visits, only: [:show, :new]
   end
-  resources :users do
-    resources :dogs, only: [:index, :new, :create]
-  end
-  resources :dogs, only: [:index, :show, :create]
+  resources :users
+  resources :dogs
+  resources :visits  
+  resources :playdates
 
-   resources :visits do 
-     resources :playdates
-   end
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    #For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
