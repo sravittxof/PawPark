cadc7c6b39f8ea113e63263d9e12e7f024e5cfe3
sravittxof <travis.fox90@gmail.come>
2021-03-18 21:39:45 -0400

    skip_before_action for new and create actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :redirect_if_not_logged_in
+    skip_before_action :redirect_if_not_logged_in, only: [:new, :create]
         
     def new
         @user = User.new
