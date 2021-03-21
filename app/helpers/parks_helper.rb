ebfdfaec3e0c75262f3dd244129a929d8d9b89fb
sravittxof <travis.fox90@gmail.come>
2021-03-20 22:12:28 -0400

    Define count_dogs_at_park method. Define and then comment out additional methods.

--- app/helpers/parks_helper.rb
+++ app/helpers/parks_helper.rb
@@ -1,17 +1,35 @@
 module ParksHelper
 
-    def at_park?(park)
-        my_current_visits.any? { |v| v.park_id == park.id } 
+    def at_park?(dogs)
+        dogs.any? { |dog| dog.user == current_user }
     end
 
-    def my_current_visits_at_this_park(park)
-        my_current_visits.find_all { |v| v.park_id == park.id }
+    # def my_current_visits_at_this_park(park)
+    #     my_current_visits.find_all { |v| v.park_id == park.id }
+    # end
+
+    # def check_out_of_park(park)
+    #     if park.dogs_at_park.where(user: current_user) #.any? { |dog| dog.my_dog?(current_user) }
+    #         link_to "Check-Out of Park", edit_visit_path(dog.visits.last)
+    #     end
+    # end
+
+    # def show_dogs_at_park(park)
+    #     park.dogs_at_park.each do |dog|
+    #         link_to dog.name, dog_path(dog)
+    #     end
+    # end
+
+    # def dogs_not_at_park
+    #     dogs = my_dogs
+    # end
+
+
+
+    def count_dogs_at_park(park)
+        park.dogs_at_park.count
     end
 
-    def check_out_of_park(park)
-        if at_park?(park)
 
-        end
-    end
 
 end
