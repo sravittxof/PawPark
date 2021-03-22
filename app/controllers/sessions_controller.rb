d74019dfe8c30fc24d2cc6c8d6a0872899573fe9
sravittxof <travis.fox90@gmail.come>
2021-03-21 21:49:15 -0400

    Removed pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,6 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(username: params[:username])
-        binding.pry
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             redirect_to root_path
