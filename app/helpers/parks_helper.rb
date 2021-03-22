97d7e009edc53e6d47b322042952b02bae4f0e9c
sravittxof <travis.fox90@gmail.come>
2021-03-22 16:39:10 -0400

    remove commented out code

--- app/helpers/parks_helper.rb
+++ app/helpers/parks_helper.rb
@@ -1,32 +1,7 @@
 module ParksHelper
 
-    def at_park?(dogs)
-        dogs.any? { |dog| dog.user == current_user }
-    end
 
-    # def my_current_visits_at_this_park(park)
-    #     my_current_visits.find_all { |v| v.park_id == park.id }
-    # end
 
-    # def check_out_of_park(park)
-    #     if park.dogs_at_park.where(user: current_user) #.any? { |dog| dog.my_dog?(current_user) }
-    #         link_to "Check-Out of Park", edit_visit_path(dog.visits.last)
-    #     end
-    # end
-
-    # def show_dogs_at_park(park)
-    #     park.dogs_at_park.each do |dog|
-    #         link_to dog.name, dog_path(dog)
-    #     end
-    # end
-
-    # def dogs_not_at_park
-    #     dogs = my_dogs
-    # end
-
-    # def user_dog_visits_at_park(park)
-    #     Visit.joins(dog: :user).where({visits: {active_visit: true, park: park}})
-    # end
 
     def active_visits_by_park_dog_user(park)
         Visit.active_visit.joins(dog: :user).where({ park: park, dog: {user: current_user} })
