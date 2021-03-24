0aeaf686bed2bafec6d8dc5c9e9f8b8bcf2cb665
sravittxof <travis.fox90@gmail.come>
2021-03-23 21:07:25 -0400

    create parks#most_visits controller action

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -7,7 +7,10 @@ class ParksController < ApplicationController
     
     def show
         @park = Park.find(params[:id])
-        #@dogs_at_park = dogs_at_park(@park)
+    end
+
+    def most_visits
+        @park = Park.find_by_sql("SELECT * FROM parks INNER JOIN visits ON visit.park_id = park.id GROUP BY park.id ORDER BY COUNT(visits.id) DESC")
     end
 
     def edit
